cf459f54542267144dd1e1da8f7695599ddf0de1
Marco Singhof <singhofmarco@gmail.com>
2023-12-29 10:01:31 +0100

    Add detail modal to recipe card

--- components/recipe-card.tsx
+++ components/recipe-card.tsx
@@ -1,10 +1,17 @@
-import { EdamamRecipe } from "@/types";
+import { EdamamIngredient, EdamamRecipe } from "@/types";
+import { Button } from "@nextui-org/button";
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@nextui-org/modal";
 import { Image } from "@nextui-org/image";
+import { Link } from "@nextui-org/link";
 
 export const RecipeCard = ({ recipe }: { recipe: EdamamRecipe }) => {
+    const {isOpen, onOpen, onClose} = useDisclosure();
+
     return (
-        <Card className="col-span-12 sm:col-span-4 h-[300px]"
+        <>
+        <Card className="col-span-12 sm:col-span-3 h-[300px]"
+            onClick={() => onOpen()}
             isPressable
             isFooterBlurred>
             <Image
@@ -22,5 +29,46 @@ export const RecipeCard = ({ recipe }: { recipe: EdamamRecipe }) => {
                 <h4 className="text-white font-medium text-large">{ recipe.label }</h4>
             </CardFooter>
         </Card>
+        <Modal backdrop='blur' isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">{ recipe.label }</ModalHeader>
+              <ModalBody>
+
+              <div className="flex gap-4 items-center">
+                    <Image
+                        removeWrapper
+                        alt="Card background"
+                        className="z-0 w-25 object-cover"
+                        src={recipe.image}
+                        width={75}
+                        height={125}
+                    />
+                    <div className="">
+                        <div>Calories: {recipe.calories.toFixed(0)} kcal</div>
+                        <div>Time: {recipe.totalTime > 0 ? `${recipe.totalTime} min` : 'N/A'}</div>
+                    </div>
+                </div>
+                <h2 className="mt-4 font-bold">Ingredients</h2>
+                <ul className="list-disc pl-4">
+                    {recipe.ingredients.map((ingredient: EdamamIngredient) => (
+                        <li key={ingredient.text}>{ingredient.text}</li>
+                    ))}
+                </ul>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="default" variant="ghost" href={recipe.url} as={Link} showAnchorIcon isExternal>
+                  Show Recipe
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Save
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+        </>
     )
 }
