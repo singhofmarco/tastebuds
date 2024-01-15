7117e073fd9ca50a39bbee57ee0f7450235ce32e
Marco Singhof <singhofmarco@gmail.com>
2024-01-15 12:17:09 +0100

    Add AiIcon component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { title, subtitle } from "@/components/primitives";
 import { Image } from "@nextui-org/image";
 import { Button } from "@nextui-org/button";
+import { AiIcon } from "@/components/icons";
 
 export default function Home() {
 	return (
@@ -25,10 +26,8 @@ export default function Home() {
 			<div className="max-w-[900px] gap-4 flex px-8">
 				<Button variant="solid" size="lg" radius="sm"
 					className="bg-gradient-to-tr from-pink-600 to-yellow-500 text-white"
-				 	endContent={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-						<path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
-					</svg>
-				}>
+				 	endContent={<AiIcon />}
+				>
 					Create a new recipe
 				</Button>
 				<Button variant="ghost" radius="sm" color="default" size="lg">
