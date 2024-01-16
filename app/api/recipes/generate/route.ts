4c31046040ee2732eae31b8dfdd692e70df585fa
Marco Singhof <singhofmarco@gmail.com>
2024-01-16 13:46:43 +0100

    Update GPT model version to 3.5 turbo 1106

--- app/api/recipes/generate/route.ts
+++ app/api/recipes/generate/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
             { role: "system", content: "You are a system to generate a usable recipe based on a request from a user. Return one recipe per request. Adhere to proven ratios of ingredients in cooking and baking. Use the metric system. You may divert from the metric system, if it makes sense for the type of ingredient like bell peppers should be counted as opposed to be weighted, yeast comes in packets. The user request might be a vague description of a dish or baked good but it could also include a list of ingredients the user wants to have incorporated in the recipe. Always output a JSON object which looks like: {  title: string, ingredients: string[], steps: string[], totalTime: string, cuisineType: string, description: string }" },
             { role: "user", content: query },
         ],
-        model: "gpt-4-1106-preview",
+        model: "gpt-3.5-turbo-1106",
         response_format: {type: "json_object"},
     })
 
