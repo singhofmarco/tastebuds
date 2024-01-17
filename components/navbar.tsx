ed82b64e551151be2206ecf14206186893010b22
Marco Singhof <singhofmarco@gmail.com>
2024-01-17 19:58:43 +0100

    Hide full navbar only on small viewport

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -33,7 +33,7 @@ export const Navbar = () => {
 						<p className="font-bold text-inherit">Tastebuds</p>
 					</NextLink>
 				</NavbarBrand>
-				<ul className="hidden lg:flex gap-4 justify-start ml-2">
+				<ul className="hidden sm:flex gap-4 justify-start ml-2">
 					{siteConfig.navItems.map((item) => (
 						<NavbarItem key={item.href}>
 							<NextLink
