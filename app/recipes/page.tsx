b2fc45a63af60523bb5e9aa2e0556a9769ad3b66
Marco Singhof <singhofmarco@gmail.com>
2024-01-19 14:26:45 +0100

    Refactor recipe search functionality

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -4,11 +4,19 @@ import { RecipeSearch } from "@/components/recipe-search";
 import { Recipe } from "@prisma/client"
 import prisma from "../lib/prisma";
 
-export default async function RecipesPage() {
+export default async function RecipesPage({ searchParams }: { searchParams:  { [key: string]: string | string[] | undefined } }) {
+	const query = searchParams.query?.toString()
 	const savedRecipes = await prisma.recipe.findMany({
 		orderBy: {
 			createdAt: 'desc'
 		}
+	})
+
+	const filteredRecipes = savedRecipes.filter((recipe: Recipe) => {
+		if (!query) return true
+
+		return recipe.title.toLowerCase().includes(query.toLowerCase())
+			|| recipe.cuisineType.toLowerCase().includes(query.toLowerCase())
 	})
 
 	return (
@@ -16,10 +24,17 @@ export default async function RecipesPage() {
 			<div className="text-center">
 				<h1 className={title()}>Recipes</h1>
 			</div>
-			<RecipeSearch savedRecipes={savedRecipes}>
-				{savedRecipes.map((recipe: Recipe) => (
-					<RecipeCard key={recipe.title} recipe={recipe} />
-				))}
+			<RecipeSearch>
+				<>
+					{filteredRecipes.length === 0 && (
+						<div className="grid justify-center items-center h-full text-center">
+							<p className="text-default-400">No recipes found.</p>
+						</div>
+					)}
+					{filteredRecipes.map((recipe: Recipe) => (
+						<RecipeCard key={recipe.title} recipe={recipe} />
+					))}
+				</>
 			</RecipeSearch>
 		</div>
 	);
--- components/recipe-search.tsx
+++ components/recipe-search.tsx
@@ -2,68 +2,52 @@
 
 import { Input } from "@nextui-org/input"
 import { PlusIcon, SearchIcon } from "./icons"
-import { useEffect, useState } from "react"
 import { Card, CardFooter } from "@nextui-org/card"
 import AddRecipeModal from "./add-recipe-modal"
 import { useDisclosure } from "@nextui-org/modal"
-import { RecipeCard } from "./recipe-card"
-import { Recipe } from "@prisma/client"
+import { useSearchParams } from "next/navigation"
+import { useRouter } from "next/navigation"
 
-export const RecipeSearch = ({children, savedRecipes}: {children: React.ReactNode[], savedRecipes: any}) => {
-    const [query, setQuery] = useState<string>('')
-	const [recipes, setRecipes] = useState<Recipe[]>([])
+export const RecipeSearch = ({ children }: { children: React.ReactElement }) => {
 	const {isOpen: isAddRecipeModalOpen, onOpen: onAddRecipeModalOpen, onClose: onAddRecipeModalClose} = useDisclosure()
+	const searchParams = useSearchParams()
+	const params = new URLSearchParams(searchParams.toString())
+	const router = useRouter()
 
-	useEffect(() => {
-		if (query.length) {
-			const filteredRecipes = savedRecipes.filter((recipe: Recipe) => {
-				return recipe.title.toLowerCase().includes(query.toLowerCase()) || recipe.cuisineType.toLowerCase().includes(query.toLowerCase())
-			})
-			setRecipes(filteredRecipes)
+	function updateQuery(query: string) {
+		if (!query.length) {
+			params.delete('query')
 		} else {
-			setRecipes(savedRecipes)
+			params.set('query', query)
 		}
-	}, [query, savedRecipes])
 
-	const filteredRecipes = (
-		recipes.map((recipe: Recipe) => (
-			<RecipeCard key={recipe.title} recipe={recipe} />
-		))
-	)
+		router.push("/recipes?" + params.toString())
+	}
 
     return (
         <div className="mt-8 flex flex-col gap-y-4 px-8">
-			<form
-				className="flex-1"
-				onSubmit={(e) => { e.preventDefault() }}
-			>
-				<div className="flex gap-4">
-					<Input
-						aria-label="Search"
-						classNames={{
-							inputWrapper: "bg-default-100",
-							input: "text-sm",
-						}}
-						labelPlacement="outside"
-						placeholder="Search..."
-						startContent={
-							<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-						}
-						type="search"
-						autoComplete="off"
-						onChange={(e) => {
-							setQuery(e.target.value)
-						}}
-					/>
-				</div>
-			</form>
-			{(query.length > 0 && filteredRecipes.length === 0) && (
-				<div className="grid justify-center items-center h-full text-center">
-					<p className="text-default-400">No recipes found.</p>
-				</div>
-			)}
+			<div className="flex flex-1 gap-4">
+				<Input
+					aria-label="Search"
+					classNames={{
+						inputWrapper: "bg-default-100",
+						input: "text-sm",
+					}}
+					labelPlacement="outside"
+					placeholder="Search..."
+					startContent={
+						<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+					}
+					type="search"
+					autoComplete="off"
+					defaultValue={params.get('query') || ''}
+					onChange={(e) => {
+						updateQuery(e.target.value)
+					}}
+				/>
+			</div>
 			<ul className="gap-4 grid grid-cols-12 grid-rows-2">
-				{ query.length ? filteredRecipes : children }
+				{ children }
 				<Card
 					className="col-span-12 sm:col-span-3 h-[300px] group"
 					onPress={onAddRecipeModalOpen}
