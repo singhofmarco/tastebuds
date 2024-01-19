ffacf55f0b6f088fe5530a83f55957a15ab37889
Marco Singhof <singhofmarco@gmail.com>
2024-01-19 11:08:13 +0100

    Refactor layout component
    Delete recipes layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
 						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+						<main className="container mx-auto max-w-7xl py-8 sm:py-12 flex-grow">
 							{children}
 						</main>
 					</div>
