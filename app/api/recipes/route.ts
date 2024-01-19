acb39cd154fef9ffd266110407c4783c2c4e5e60
Marco Singhof <singhofmarco@gmail.com>
2024-01-19 12:15:32 +0100

    Remove unused route handler

--- app/api/recipes/route.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
-
-export async function GET(request: Request) {
-    const recipes = await prisma.recipe.findMany()
-
-    return Response.json(recipes)
-}
