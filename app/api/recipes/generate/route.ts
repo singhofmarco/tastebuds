713c58a7144874c7c1eebd00837e30d0135e63eb
Marco Singhof <singhofmarco@gmail.com>
2024-01-19 15:11:44 +0100

    Update runtime configuration

--- app/api/recipes/generate/route.ts
+++ app/api/recipes/generate/route.ts
@@ -1,8 +1,6 @@
 import OpenAI from "openai"
 
-export const config = {
-    runtime: 'edge', // 'nodejs' is the default
-}
+export const runtime = 'edge'
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
