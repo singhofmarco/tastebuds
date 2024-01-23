bbc06345f7bfd5165074da6cde9ebb307e0b7614
Marco Singhof <singhofmarco@gmail.com>
2024-01-23 18:26:02 +0100

    Use singleton prisma client if not in production env

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,26 +1,37 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient().$extends({
-	result: {
-		recipe: {
-		  saved: {
-			compute() {
-			  return true
-			},
-		  },
-		},
-	  },
-})
-.$extends({
-	result: {
-		recipe: {
-			isGeneratingImage: {
-				compute(recipe) {
-					return recipe.qStashMessageId !== null
-				},
-			},
-		}
-	}
-})
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+    .$extends({
+      result: {
+        recipe: {
+          saved: {
+            compute() {
+              return true
+            },
+          },
+        },
+      },
+    })
+    .$extends({
+      result: {
+        recipe: {
+          isGeneratingImage: {
+            compute(recipe) {
+              return recipe.qStashMessageId !== null
+            },
+          },
+        },
+      },
+    })
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export default prisma
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
