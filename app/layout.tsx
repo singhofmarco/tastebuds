1c6b196958afc6e69a1c5c636acf051fd7821215
Marco Singhof <singhofmarco@gmail.com>
2024-02-04 18:13:05 +0100

    wip placeholder

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import { Metadata } from "next";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Providers } from "./providers";
-import { Navbar } from "@/components/navbar";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
 export const metadata: Metadata = {
@@ -40,7 +38,6 @@ export default function RootLayout({
 			>
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
-						<Navbar />
 						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
 							{children}
 						</main>
--- config/site.ts
+++ config/site.ts
@@ -1,46 +1,6 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Tastebuds",
-	description: "Cook together without the hassle.",
-	navItems: [
-		{
-		label: "Recipes",
-		href: "/recipes",
-		},
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
+	name: "Tastebuds AI",
+	description: "Coming soon...",
 };
