098b71e861077bf0c97e41b1e185a904b2e1a071
Marco Singhof <singhofmarco@gmail.com>
2024-02-24 19:56:39 +0100

    Polish recipes card grid and image placeholders

--- components/recipe-card.tsx
+++ components/recipe-card.tsx
@@ -8,7 +8,7 @@ export const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
   return (
     <Link
       href={`/recipes/${recipe.id}`}
-      className="col-span-12 sm:col-span-3 h-[300px]"
+      className="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 h-[300px]"
     >
       <Card className="h-full group select-none" isFooterBlurred>
         {recipe.image ? (
@@ -20,26 +20,29 @@ export const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
             height={625}
           />
         ) : (
-          <CardBody className="p-4 flex space-y-5">
-            <div className="flex justify-between">
-              <div className="h-24 w-24 rounded-lg bg-default-200/50"></div>
+          <div className="p-4 flex flex-col space-y-5">
+            <div className="grid grid-cols-2 gap-x-4 justify-between">
+              <div className="h-full w-full max-w-32 aspect-square rounded-lg bg-default-200/50"></div>
               <div className="space-y-3">
-                <div className="h-3 w-32 rounded-lg bg-default-100"></div>
-                <div className="h-3 w-12 rounded-lg bg-default-100"></div>
-                <div className="flex space-x-2">
-                  <div className="h-3 w-7 rounded-md bg-default-200/70"></div>
-                  <div className="h-3 w-7 rounded-md bg-default-200/70"></div>
+                <div className="h-3 max-w-32 rounded-lg bg-default-100"></div>
+                <div className="h-3 max-w-12 rounded-lg bg-default-100"></div>
+                <div className="flex space-x-2 max-w-14">
+                  <div className="h-3 w-full rounded-md bg-default-200/70"></div>
+                  <div className="h-3 w-full rounded-md bg-default-200/70"></div>
                 </div>
 
                 <div className="h-6 w-full rounded-lg bg-default-100"></div>
               </div>
             </div>
-            <div className="space-y-3 flex flex-col items-end">
-              <div className="h-3 w-32 rounded-lg bg-default-100"></div>
-              <div className="h-3 w-32 rounded-lg bg-default-100"></div>
-              <div className="h-3 w-32 rounded-lg bg-default-100"></div>
+            <div className="grid grid-cols-2 gap-x-4 justify-between">
+              <div></div>
+              <div className="space-y-3 flex flex-col">
+                <div className="h-3 w-full rounded-lg bg-default-100"></div>
+                <div className="h-3 w-full rounded-lg bg-default-100"></div>
+                <div className="h-3 w-full rounded-lg bg-default-100"></div>
+              </div>
             </div>
-          </CardBody>
+          </div>
         )}
         <CardFooter className="absolute z-10 bottom-0 flex-col !items-start">
           <p
